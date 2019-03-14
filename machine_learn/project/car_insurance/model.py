7678497dcd059222d933d413ef447f4a62f6fb12
ksiailaohuyou <AR_VR_TJ@163.COM>
2019-03-14 11:21:03 +0800

    汽车保险案例   tensorflow 队列

--- machine_learn/project/car_insurance/model.py
+++ machine_learn/project/car_insurance/model.py
@@ -1,6 +1,65 @@
 import   pandas  as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.feature_extraction import DictVectorizer
+import numpy  as np
+from sklearn.linear_model import LinearRegression, SGDRegressor,  Ridge, LogisticRegression
+from sklearn.metrics import mean_squared_error, classification_report
+from sklearn.model_selection import  GridSearchCV
+from sklearn.tree import DecisionTreeClassifier,export_graphviz
 
-train_data=pd.read_csv('./train.csv')
-train_data.head()
+from   sklearn.ensemble  import RandomForestClassifier
+from sklearn.preprocessing import PolynomialFeatures
 
-train_data.exc
+from sklearn.decomposition import PCA
+
+
+
+def  test1():
+    train_data = pd.read_csv('./train.csv')
+
+    X_data = train_data.iloc[:, 2:]
+    y_data = train_data['Score']
+
+    dict = DictVectorizer(sparse=False)
+    X_data_dict = dict.fit_transform(X_data.to_dict(orient="records"))
+
+
+    # 特征缩减到百分之九十
+    pca = PCA(n_components=0.95)
+    data = pca.fit_transform(X_data_dict)
+
+    std_x = StandardScaler()
+    X_data_dict = std_x.fit_transform(data)
+
+    X_data_dict_poly = X_data_dict
+    x_train_poly, x_test_poly, y_train_poly, y_test_poly = train_test_split(X_data_dict_poly, y_data, test_size=0.25)
+    param = {"penalty": ['l2', 'elasticnet'], "max_iter": [30000, 20000, 15000]}
+    reg = SGDRegressor()
+    # 网格搜索与交叉验证
+    gc = GridSearchCV(reg, param_grid=param, cv=3)
+    #     print(x_train_poly, y_train_poly)
+    gc.fit(x_train_poly, y_train_poly)
+    y_sgd_predict = gc.predict(x_test_poly)
+    print("梯度下降的均方误差：", mean_squared_error(y_test_poly, y_sgd_predict))
+    # print("准确率：", gc.score(x_test_poly, y_test_poly))
+
+    print("查看选择的参数模型：", gc.best_params_)
+    print("选择最好的模型是：", gc.best_estimator_)
+
+    #预测
+    test_data = pd.read_csv('./test.csv')
+    X_test_data = test_data.iloc[:, 2:]
+
+    X_test_data_dict = dict.transform(X_test_data.to_dict(orient="records"))
+    X_test_data_dict = pca.transform(X_test_data_dict)
+    X_test_data_dict = std_x.transform(X_test_data_dict)
+    X_test_data_dict_poly = X_test_data_dict
+    y_gc_predict = gc.predict(X_test_data_dict_poly)
+    submission_data = pd.read_csv('./submission.csv')
+    submission_data['Id'] = test_data['Id']
+    submission_data['Score'] = np.rint(y_gc_predict).astype(np.int)
+    submission_data.to_csv('submission.csv', header=True)
+
+if __name__=='__main__':
+    test1()
--- tf/others/queue.py
+++ tf/others/queue.py
@@ -1,6 +1,10 @@
 import tensorflow as tf
 import os
+# 定义cifar的数据等命令行参数
+FLAGS = tf.app.flags.FLAGS
 
+tf.app.flags.DEFINE_string("cifar_dir", "./data/cifar10/cifar-10-batches-bin/", "文件的目录")
+tf.app.flags.DEFINE_string("cifar_tfrecords", "./tmp/cifar.tfrecords", "存进tfrecords的文件")
 def  test01():
     # 模拟一下同步先处理数据，然后才能取数据训练
     # tensorflow当中，运行操作有依赖性
@@ -71,6 +75,99 @@ def  test02():
         coord.request_stop()
 
         coord.join(threads)
+def  csvread(filelist):
+    """
+    读取CSV文件
+    :param filelist: 文件路径+名字的列表
+    :return: 读取的内容
+    """
+    # 1、构造文件的队列
+    file_queue=tf.train.string_input_producer(filelist)
+
+    # 2、构造csv阅读器读取队列数据（按一行）
+    reader=tf.TextLineReader()
+    key,value=reader.read(file_queue)
+
+    # 3、对每行内容解码
+    # record_defaults:指定每一个样本的每一列的类型，指定默认值[["None"], [4.0]]
+    records=[['None'],['None']]
+
+    example, label = tf.decode_csv(value, record_defaults=records)
+
+
+
+def picread(filelist):
+    """
+    读取狗图片并转换成张量
+    :param filelist: 文件路径+ 名字的列表
+    :return: 每张图片的张量
+    """
+    # 1、构造文件队列
+    file_queue = tf.train.string_input_producer(filelist)
+
+    # 2、构造阅读器去读取图片内容（默认读取一张图片）
+    reader = tf.WholeFileReader()
+
+    key, value = reader.read(file_queue)
+
+    print(value)
+
+    # 3、对读取的图片数据进行解码
+    image = tf.image.decode_jpeg(value)
+
+    print(image)
+
+    # 5、处理图片的大小（统一大小）
+    image_resize = tf.image.resize_images(image, [200, 200])
+
+    print(image_resize)
+
+    # 注意：一定要把样本的形状固定 [200, 200, 3],在批处理的时候要求所有数据形状必须定义
+    image_resize.set_shape([200, 200, 3])
+
+    print(image_resize)
+
+    # 6、进行批处理
+    image_batch = tf.train.batch([image_resize], batch_size=20, num_threads=1, capacity=20)
+
+    print(image_batch)
+
+    return image_batch
+
 if __name__=="__main__":
     # test01()
-    test02()
+    # test02()
+    # 1、找到文件，放入列表   路径+名字  ->列表当中
+    file_name = os.listdir(FLAGS.cifar_dir)
+
+    filelist = [os.path.join(FLAGS.cifar_dir, file) for file in file_name if file[-3:] == "bin"]
+
+    # print(file_name)
+    cf = CifarRead(filelist)
+
+    # image_batch, label_batch = cf.read_and_decode()
+
+    image_batch, label_batch = cf.read_from_tfrecords()
+
+    # 开启会话运行结果
+    with tf.Session() as sess:
+        # 定义一个线程协调器
+        coord = tf.train.Coordinator()
+
+        # 开启读文件的线程
+        threads = tf.train.start_queue_runners(sess, coord=coord)
+
+        # 存进tfrecords文件
+        # print("开始存储")
+        #
+        # cf.write_ro_tfrecords(image_batch, label_batch)
+        #
+        # print("结束存储")
+
+        # 打印读取的内容
+        print(sess.run([image_batch, label_batch]))
+
+        # 回收子线程
+        coord.request_stop()
+
+        coord.join(threads)
